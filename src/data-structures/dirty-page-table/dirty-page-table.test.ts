c37b7d63406482565249a9d251fdf99103abdce3
German Robayo Paz <germanrobayo33@gmail.com>
2019-04-30 21:00:55 -0400

    add test for DPT.add

--- src/data-structures/dirty-page-table/dirty-page-table.test.ts
+++ src/data-structures/dirty-page-table/dirty-page-table.test.ts
@@ -10,4 +10,14 @@ describe(tadName, () => {
         expect(dirtyPageTable.size()).toEqual(0)
     })
 
+    it('should should be of size 1 when it\'s empty and a new entry is added', () => {
+        const entry: DirtyPageEntry = {
+            pageId: 'P1000',
+            recLSN: 10000
+        }
+        const dirtyPageTable: DirtyPageTable = new DirtyPageTable()
+        dirtyPageTable.add(entry)
+        expect(dirtyPageTable.size()).toEqual(1)
+    })
+
 })
